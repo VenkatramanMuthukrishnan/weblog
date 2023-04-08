1a880ee177ff5339a493316d7448b507c7dd51ad
Venkatraman Muthukrishnan <me@venkatraman.co.in>
2023-04-08 11:48:53 +0530

    basic working

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,19 @@
-import React, { useState, useEffect } from "react";
-import ReactQuill from "react-quill";
+import React, { useState, useEffect, useRef } from "react";
+import ReactQuill,{Quill} from "react-quill";
 import Prism from "prismjs";
 import html_beautify from "js-beautify";
-import { modules, formats } from "./constants";
-
+import EditorToolbar, { modules, formats } from "./EditorToolbar";
 import "react-quill/dist/quill.snow.css";
 import "prismjs/themes/prism.css";
-
-// const Quill = ReactQuill.Quill;
-// Quill.register(Quill.import("attributors/style/align"), true);
-
-const defaultContent = "";
-
-const CustomButton = () => <span className="octicon octicon-star" />;
-
-/*
- * Custom toolbar component including insertStar button and dropdowns
- */
-const CustomToolbar = () => (
-  <div id="toolbar">
-    <select
-      className="ql-header"
-      defaultValue={""}
-      onChange={(e) => e.persist()}
-    >
-      <option value="1"></option>
-      <option value="2"></option>
-      <option selected></option>
-    </select>
-    <button className="ql-bold"></button>
-    <button className="ql-italic"></button>
-    <select className="ql-color">
-      <option value="red"></option>
-      <option value="green"></option>
-      <option value="blue"></option>
-      <option value="orange"></option>
-      <option value="violet"></option>
-      <option value="#d0d1d2"></option>
-      <option selected></option>
-    </select>
-    <button className="ql-insertStar">
-      <CustomButton />
-    </button>
-  </div>
-);
+import katex from "katex";
+import "katex/dist/katex.min.css";
+(window as any).katex = katex;
 
 const Editor = () => {
-  const [value, setValue] = useState(defaultContent);
+  const [value, setValue] = useState('');
+  const inputRef = useRef<ReactQuill>(null);
 
-  const handlePaste = (clipboardData: any, editor: any) => {
-    // Manipulate clipboard data here before pasting
-    editor.clipboard.dangerouslyPasteHTML(clipboardData.getData("text/html"));
-  };
+ 
 
   const onEditorChange = (value: any) => {
     setValue(value);
@@ -70,10 +32,12 @@ const Editor = () => {
 
   return (
     <div className="text-editor">
+            <EditorToolbar />
       <ReactQuill
             value={value}
             modules={modules}
                     formats={formats}
+                    ref={inputRef}
         onChange={onEditorChange}
       />
       <pre className="preview">
